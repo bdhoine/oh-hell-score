923ecea43fefbbda3cbc62154d6d5249717316e2
“je34791” <nick.vanvynckt@kbc.be>
2018-06-24 22:51:46 +0200

    Added email/password authentication and google authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { NewGamePage } from '../pages/new-game/new-game';
+import { LoginPage } from '../pages/login/login';
+import { AuthService } from '../providers/authentication/auth.service';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = NewGamePage;
+  rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private authService: AuthService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
